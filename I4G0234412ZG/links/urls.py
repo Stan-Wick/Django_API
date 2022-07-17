41c93ecffbe0efb0085967336935a41a8d31433e
Stan-Wick <105384071+Stan-Wick@users.noreply.github.com>
2022-07-17 15:31:38 +0100

    Querysets and managers

--- I4G0234412ZG/links/urls.py
+++ I4G0234412ZG/links/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views 
+from . import views
 
 app_name="link"
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path("update/<int:pk>", views.PostUpdateApi.as_view(), name="api_update"),
     path("delete/<int:pk>", views.PostDeleteApi.as_view(), name="api_delete"),
     path("", views.PostListApi.as_view(), name="api_list"),
+    path("active/", views.ActiveLinkView.as_view(), name='active_link'),
+    path("recent/", views.RecentLinkView.as_view(), name='recent_link'),
 ]
